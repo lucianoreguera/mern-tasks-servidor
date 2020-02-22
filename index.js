8fd7abe1df86e3b5dec4e1348e2de3e4e575e9d4
Luciano <regueraluciano@gmail.com>
2020-02-22 09:21:15 -0300

    Cambios en PORT

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-// Crear el servidor
+// crear el servidor
 const app = express();
 
-// Conectar a la BD
+// Conectar a la base de datos
 conectarDB();
 
-// Habilitar cors
+// habilitar cors
 app.use(cors());
 
-// Habilitar express.json para poder leer datos que el usuario ingresa
-app.use( express.json({extended: true }));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-// Puerto de la app
-const port = process.env.port || 4000;
+// puerto de la app
+const port = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-// Arrancar la app
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`);
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
